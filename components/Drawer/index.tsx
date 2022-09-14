92c529ab92b81e8e56545f26e924b54da99ba11d
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-14 16:49:03 +0900

    💄 Design: Drawer 퍼블리싱 및 애니메이션 효과 적용

---
+++ components/Drawer/index.tsx
@@ -0,0 +1,74 @@
+import { FC, useEffect, useState } from "react";
+
+interface Props {
+  onClick: any;
+}
+
+const Drawer: FC<Props> = ({ onClick }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const menus = [
+    "배터리 케어 서비스",
+    "전기차 충전소 정보",
+    "전기차 이야기",
+    "마이페이지",
+    "고객센터",
+    "설정",
+  ];
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setTimeout(() => {
+      onClick();
+    }, 300);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(true);
+    });
+  }, []);
+
+  return (
+    <aside>
+      <div className="fixed z-30 bg-black opacity-30 w-full h-full left-0 top-0"></div>
+      <div
+        className={`fixed z-40 h-screen p-4 overflow-y-auto bg-white w-[19rem] top-0 right-0 transform ease-in-out transition-all duration-300 
+      ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="flex flex-col h-full justify-between">
+          <button
+            type="button"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center "
+            onClick={handleClose}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close</span>
+          </button>
+          <ul className="space-y-[1rem] pt-[4rem]">
+            {menus.map((menu, index) => (
+              <li key={index} className="text-[1.2rem]">
+                {menu}
+              </li>
+            ))}
+          </ul>
+          <span className="text-gray-500">서비스 이용약관 • 개인정보 처리방침</span>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Drawer;
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,15 @@
+import Drawer from "components/Drawer";
 import useScrollPosition from "hooks/useScrollPosition";
 import Image from "next/image";
+import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
   const scrollPosition = useScrollPosition();
-  console.log(scrollPosition);
+  const [showDrawer, setShowDrawer] = useState<boolean>(false);
+  const handleToggleDrawer = () => {
+    setShowDrawer(!showDrawer);
+  };
 
   return (
     <header className="sticky top-0">
@@ -13,7 +18,8 @@ const Header = () => {
         ${scrollPosition >= 20 && "shadow"} `}
       >
         <Image src="/images/logo.png" alt="로고" width={60} height={24} />
-        <FiMenu className="text-[1.3rem]" />
+        <FiMenu className="text-[1.3rem]" onClick={handleToggleDrawer} />
+        <div className="absolute">{showDrawer && <Drawer onClick={handleToggleDrawer} />}</div>
       </div>
     </header>
   );
