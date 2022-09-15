ae42c790b9f9acb9ac465413d40ab3d3a6b1f97b
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-15 11:37:37 +0900

    💄 Design: Bottom safe-area 퍼블리싱 처리

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,6 +2,7 @@ import CheckBox from "components/Forms/Checkbox";
 import Input from "components/Forms/Input";
 import Section from "layouts/Section";
 import { NextPage } from "next";
+import Head from "next/head";
 import { useForm } from "react-hook-form";
 import { SignUpForm } from "types/forms/signup";
 
@@ -23,6 +24,9 @@ const SignupPage: NextPage = () => {
 
   return (
     <Section>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      </Head>
       <form autoComplete="off" onSubmit={handleSubmit(onValid)}>
         <h1 className="text-3xl font-bold mb-[0.5rem]">회원가입</h1>
         <h3 className="text-sm text-gray-500 mb-[4rem]">
@@ -63,6 +67,13 @@ const SignupPage: NextPage = () => {
             이메일 수신 동의
           </CheckBox>
           <span className="text-gray-400 text-sm">* 수신 동의 시 차량 맞춤형 정보를 얻으실 수 있습니다.</span>
+        </div>
+
+        <div className="fixed left-0 bottom-[env(safe-area-inset-bottom)] pb-safe w-full z-10">
+          <button className="w-full min-h-[3rem] bg-blue-500 text-white font-bold">가입 신청</button>
+        </div>
+        <div className="fixed left-0 bottom-0 pb-safe w-full">
+          <button className="w-full min-h-[2rem] bg-blue-500" />
         </div>
       </form>
     </Section>
