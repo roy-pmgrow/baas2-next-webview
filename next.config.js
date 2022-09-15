9a06564e5979c85d41b4457da49c8efcc8ea4c43
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-15 11:19:08 +0900

    👷 Setting: API 주소 프록시 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-}
+  experimental: {
+    externalDir: true,
+  },
+  images: {
+    // loader: "akamai",
+    // path: "/",
+    // domains: ["openweathermap.org"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
