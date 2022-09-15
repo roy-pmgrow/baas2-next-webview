1cd62b82b8804c234b824fe182fa2a26b278bf51
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-15 14:20:53 +0900

    👷 Setting: API 프록시 주소 변경

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        source: "/app/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/app/:path*`,
       },
     ];
   },
