8c602d446c13c0825bbef3f9d0ab68bd11c4f275
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-15 14:21:25 +0900

    🍄 Api: login, signup, idcheck API 적용

--- apis/user/index.ts
+++ apis/user/index.ts
@@ -1,7 +1,29 @@
 import { httpRequest } from "./../httpRequest";
 const userApi = {
-  login(loginId: string, password: string) {
-    return httpRequest.post("/api/login", { loginId, password });
+  login(loginId: string, password: string, saved: boolean, keeped: boolean) {
+    return httpRequest.post("/api/login", {
+      data: {
+        user_id: loginId,
+        user_pw: password,
+        prstnt_yn: keeped ? "Y" : "N",
+        id_save_yn: saved ? "Y" : "N",
+      },
+    });
+  },
+  signup(loginId: string, password: string) {
+    return httpRequest.post("/app/user/regUser", {
+      data: {
+        user_id: loginId, //
+        user_pw: password,
+      },
+    });
+  },
+  idCheck(loginId: string) {
+    return httpRequest.post("/app/auth/idCheck", {
+      data: {
+        user_id: loginId, //
+      },
+    });
   },
 };
 
