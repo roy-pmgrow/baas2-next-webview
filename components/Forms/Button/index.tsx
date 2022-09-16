ec1fba8991e9c80d86b271e35c6f1dab69162a09
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-16 09:55:55 +0900

    🌱 Update: Button 컴포넌트 수정

--- components/Forms/Button/index.tsx
+++ components/Forms/Button/index.tsx
@@ -1,14 +1,15 @@
 import { FC, ReactNode } from "react";
 
 interface Props {
+  className?: string;
   children?: ReactNode;
   [key: string]: any;
 }
 
-const Button: FC<Props> = ({ children, ...rest }) => {
+const Button: FC<Props> = ({ className, children, ...rest }) => {
   return (
     <button
-      className="w-full h-[3.5rem] rounded-md font-medium text-[1.1rem] bg-blue-500 text-white"
+      className={`w-full h-[3.5rem] rounded-md font-medium text-[1.1rem] bg-blue-500 text-white ${className}`}
       {...rest}
     >
       {children}
