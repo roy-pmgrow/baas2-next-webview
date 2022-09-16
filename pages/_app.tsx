e0f905080c9ea38ecc3e4b91341aec951752be9c
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-16 11:20:28 +0900

    ✨ Feat: React-Query 구성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,42 @@
 import Header from "components/Header";
 import Modal from "components/Modal";
+import { Provider } from "jotai";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "swiper/css";
 import "swiper/css/bundle";
 import "swiper/css/pagination";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            retry: 0,
+            staleTime: Infinity,
+            cacheTime: Infinity,
+          },
+        },
+      }),
+  );
+
   return (
-    <div className="w-full max-w-lg mx-auto bg-white">
-      <Header />
-      <Component {...pageProps} />
-      <Modal />
-    </div>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <div className="w-full max-w-lg mx-auto bg-white">
+          <Header />
+          <Component {...pageProps} />
+          <Modal />
+        </div>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
