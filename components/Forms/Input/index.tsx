85f9d2785260da29bed51ffa5f3b0304ce666845
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-20 17:56:55 +0900

    🌱 Update: Input 컴포넌트 수정

--- components/Forms/Input/index.tsx
+++ components/Forms/Input/index.tsx
@@ -1,33 +1,16 @@
-import { useAtom } from "jotai";
-import cloneDeep from "lodash.clonedeep";
 import { FC } from "react";
 import { IoMdCloseCircle } from "react-icons/io";
-import { appAtom } from "store";
-import { AddressType } from "types/enum";
 
 interface Props {
-  type?: AddressType;
+  onClear: () => void;
   [key: string]: any;
 }
 
-const Input: FC<Props> = ({ type, register, watch, setValue, ...rest }) => {
-  const [app, setApp] = useAtom(appAtom);
-
+const Input: FC<Props> = ({ type, register, watch, onFocus, onBlur, onClear, ...rest }) => {
   return (
     <div className="p-3 bg-gray-100 rounded-lg flex items-center">
-      <input {...register} {...rest} className="bg-gray-100 w-full" />
-      {watch && (
-        <IoMdCloseCircle
-          className="text-[1.5rem] cursor-pointer text-gray-500"
-          onClick={() => {
-            if (type && type === AddressType.source) app.source = { ...app.source, bdNm: "", address: "" };
-            else if (type && type === AddressType.destination)
-              app.destination = { ...app.destination, bdNm: "", address: "" };
-            type && setApp(cloneDeep(app));
-            setValue(register.name, "");
-          }}
-        />
-      )}
+      <input {...register} {...rest} className="bg-gray-100 w-full" onFocus={onFocus} />
+      {watch && <IoMdCloseCircle className="text-[1.5rem] cursor-pointer text-gray-500" onClick={onClear} />}
     </div>
   );
 };
